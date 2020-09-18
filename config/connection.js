22b6e4eb6c88d66faf169611d38cd7bd24cfe10d
wmt36 <wmt36@Williams-iMac.local>
2020-09-18 14:58:08 -0600

    updated code for helping hand

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,19 @@
 const mysql = require("mysql");
 
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+const connection = mysql.createConnection({
+    host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "employee",
+  database: "services_db"
+});
 
 
 connection.connect(function(err) {
